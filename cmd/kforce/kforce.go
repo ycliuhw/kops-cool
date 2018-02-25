6d0c00b57d231f7e82843b10907c2fb3fed935dd
Kelvin Liu <ycliuhw@gmail.com>
2018-02-25 23:25:56 +1100

    Kelvin: added struct CmdArgs for standardize CMD params

--- cmd/kforce/kforce.go
+++ cmd/kforce/kforce.go
@@ -1,1 +1,44 @@
 package kforce
+
+import (
+	"fmt"
+)
+
+// CmdArgs - parameters for cmds
+type CmdArgs struct {
+	Env         string
+	AccountName string
+	VpcID       string
+	Region      string
+	Debug       bool
+}
+
+// Init config files and dirs for a new cluster
+func Init(args CmdArgs) string {
+	fmt.Printf("Initing... with args -> %+v\n", args)
+	return "Done!"
+}
+
+// Build cluster template
+func Build(args CmdArgs) string {
+	fmt.Printf("Building... with args -> %+v\n", args)
+	return "Done!"
+}
+
+// Diff cluster template
+func Diff(args CmdArgs) string {
+	fmt.Printf("Diffing... with args -> %+v\n", args)
+	return "Done!"
+}
+
+// Apply cluster template
+func Apply(args CmdArgs) string {
+	fmt.Printf("Applying... with args -> %+v\n", args)
+	return "Done!"
+}
+
+// Install cluster addons
+func Install(args CmdArgs) string {
+	fmt.Printf("Installing... with args -> %+v\n", args)
+	return "Done!"
+}
