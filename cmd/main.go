00e7949ce51fb534f3aa282eb259adef65eff3f6
Kelvin Liu <ycliuhw@gmail.com>
2018-02-25 23:30:25 +1100

    Kelvin: use flag to parse argv

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"./kforce"
+)
+
+const (
+	// Author is very important
+	Author string = `Yang Kelvin Liu`
+	// Version is very important as well
+	Version string = `0.0.1`
+)
 
 func main() {
-	fmt.Printf("Hello, kforce ✌️\n")
+	fmt.Printf("\nHello, %s! Kforce is here ✌️\n%s\n\n", Author, strings.Repeat("-", 100))
+
+	argv := new(kforce.CmdArgs)
+	flag.StringVar(&argv.Env, "Env", "no Env", "one of [u|s|p|m]")
+	flag.StringVar(&argv.AccountName, "AccountName", "No AccountName", "aws account name")
+	flag.StringVar(&argv.VpcID, "VpcID", "no VpcID", "Vpc ID: vpc-xxxxx")
+	flag.StringVar(&argv.Region, "Region", "no Region", "aws region")
+	flag.BoolVar(&argv.Debug, "Debug", false, "enable debug or not")
+	flag.Parse()
+
+	kforce.Init(*argv)
+	kforce.Build(*argv)
+	kforce.Apply(*argv)
+	kforce.Install(*argv)
 }
