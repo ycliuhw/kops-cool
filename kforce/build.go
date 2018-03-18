edfc96f5af5f29f43baa65efac96faac46ba7e0e
Kelvin Liu <ycliuhw@gmail.com>
2018-03-18 11:48:07 +1100

    Kelvin: added getRequiredPaths for Build cmd

--- kforce/build.go
+++ kforce/build.go
@@ -2,18 +2,25 @@ package kforce
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 // Build - Command to build configuration/templates for a new or existing cluster
 type Build struct {
+	*State
+
 	requiredPaths []string
 }
 
-func (c *Build) exec() error {
-	fmt.Print("this is Build.exec()!")
+func (c *Build) exec(s *State) error {
+	fmt.Println("this is Build.exec()!")
 	return nil
+}
+
+func (c Build) getRequiredPaths(s *State) []string {
+	return append(s.requiredPaths, c.requiredPaths...)
 }
 
 // NewCmdBuild -
@@ -23,8 +30,10 @@ func NewCmdBuild(s *State) *cobra.Command {
 		Short: "kforce build",
 		Long:  `Deploy...`,
 		Run: func(cmd *cobra.Command, args []string) {
-			c := Build{}
-			if err := c.exec(); err != nil {
+			c := &Build{}
+			fmt.Printf("buildCmd: args -> %+v\n", strings.Join(args, " "))
+			fmt.Printf("buildCmd: state -> %s\n", s)
+			if err := BuildCMD(c)(s); err != nil {
 				exitWithError(err)
 			}
 		},
